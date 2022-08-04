6ff98a53ff6e297f467ba84c1cc399cb01a6c227
Hubert <hubert.hejnowicz@interia.pl>
2022-08-04 14:56:25 +0200

    Only little changes

--- scriptsJS/second-index.js
+++ scriptsJS/second-index.js
@@ -37,7 +37,7 @@ function removeFilter(button){
 // })
 }
 
-//Button scroll
+//Button scroll on whole page
 var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
