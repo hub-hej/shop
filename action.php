f2173a291241a9188e9efaa9b5622555acec7839
Hubert <hubert.hejnowicz@interia.pl>
2022-08-04 13:37:29 +0200

    In the making

---
+++ action.php
@@ -0,0 +1,3 @@
+Witaj użytkowniku:  <?php echo htmlspecialchars($_POST['username']); ?>. <br />
+Twój adres to: <?php echo htmlspecialchars($_POST['email']); ?> <br />
+Twój komentarz to : <?php echo htmlspecialchars($_POST['desc']); ?> 
