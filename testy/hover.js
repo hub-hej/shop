34b40e2d4bdb227fcb9e6e9b70bb469573f06d63
Hubert <hubert.hejnowicz@interia.pl>
2022-08-04 13:36:57 +0200

    Nothing interesting

---
+++ testy/hover.js
@@ -0,0 +1,9 @@
+var mySlider = new rSlider({
+    target: '#sampleSlider',
+    values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+    range: true,
+    tooltip: true,
+    scale: true,
+    labels: true,
+    set: [2010, 2013]
+});
