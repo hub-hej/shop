7607f313db6f64743c4742c77b86b6e2862b786a
Hubert <hubert.hejnowicz@interia.pl>
2022-08-05 10:11:46 +0200

    Connection html with phpmyadmin

--- action.php
+++ action.php
@@ -1,3 +1,32 @@
-Witaj użytkowniku:  <?php echo htmlspecialchars($_POST['username']); ?>. <br />
+<!-- Witaj użytkowniku:  <?php echo htmlspecialchars($_POST['username']); ?>. <br />
 Twój adres to: <?php echo htmlspecialchars($_POST['email']); ?> <br />
-Twój komentarz to : <?php echo htmlspecialchars($_POST['desc']); ?> 
+Twój komentarz to : <?php echo htmlspecialchars($_POST['desc']); ?>  -->
+
+<?php
+require ('db.php');
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $comment = $_POST['comment'];
+
+    $query = "INSERT INTO `comments` (name, email, comment)
+    VALUES ('$username', '$email', '$comment')";
+    $stmt= $pdo->prepare($query);
+    $stmt->execute([$username, $email, $comment]);
+
+    if($stmt){
+        echo "<script>alert('Comment added successfully.')</script>";
+    } else{
+        echo "<script>alert('Comment does not add.')</script>";
+    }
+}
+?>
+<!-- //     $result = mysqli_query($pdo, $query);
+
+//     if($result){
+//         echo "<script>alert('Comment added successfully.')</script>";
+//     } else{
+//         echo "<script>alert('Comment does not add.')</script>";
+//     }
+// } -->
