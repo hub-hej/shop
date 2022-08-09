0c5158c646ed1193228c4089e86e1b3f72f06e3c
Hubert <hubert.hejnowicz@interia.pl>
2022-08-09 11:56:14 +0200

    Add function for input range with size

--- scriptsJS/second-index.js
+++ scriptsJS/second-index.js
@@ -92,3 +92,37 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 })();
+
+//Add function which allow you to use input range
+window.onload = function(){
+  slideOne();
+  slideTwo();
+}
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+function slideOne(){
+  if(parseInt(sliderTwo.value) - parseInt
+  (sliderOne.value) <= minGap){
+      sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.textContent = sliderOne.value;
+  fillColor();
+}
+function slideTwo(){
+  if(parseInt(sliderTwo.value) - parseInt
+  (sliderOne.value) <= minGap){
+      sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.textContent = sliderTwo.value;
+  fillColor();
+}
+function fillColor(){
+  percent1 = (sliderOne.value / sliderMaxValue) * 100;
+  percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+  sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #f74137 ${percent1}% , #f74137 ${percent2}% , #dadae5 ${percent2}%`;
+}
