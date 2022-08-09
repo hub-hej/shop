00347c2060ad563d06f35728daf242bc2ececeac
Hubert <hubert.hejnowicz@interia.pl>
2022-08-09 10:33:00 +0200

    Add new function including with hide nad show

--- scriptsJS/second-index.js
+++ scriptsJS/second-index.js
@@ -75,3 +75,20 @@ links.forEach(function (item) {
   });
 })
 })();
+
+(function (){
+//This function allow for hide and show more information in dropdown left menu (GNIAZKA, WYŁĄCZNIKI)
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+})();
