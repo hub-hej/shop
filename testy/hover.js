78dff10f023754726111e0976365f013cd8b6027
Hubert <hubert.hejnowicz@interia.pl>
2022-08-09 10:33:30 +0200

    Next test before to the introduction

--- testy/hover.js
+++ testy/hover.js
@@ -1,9 +1,3 @@
-var mySlider = new rSlider({
-    target: '#sampleSlider',
-    values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
-    range: true,
-    tooltip: true,
-    scale: true,
-    labels: true,
-    set: [2010, 2013]
+$('[data-click]').on('click', function (e) {
+    $( $(this).data('click') ).trigger('click');
 });
