96c88276641fe098b223453b321417ae58d22729
Hubert <hubert.hejnowicz@interia.pl>
2022-08-12 10:50:45 +0200

    Responsive page and add function for stars

--- scriptsJS/third-index.js
+++ scriptsJS/third-index.js
@@ -1,65 +1,66 @@
-(function (){
-const numbers = document.querySelectorAll('.tile-product .number');
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        const plus = number.querySelector('.plus');
-        const minus = number.querySelector('.minus');
-        const input = number.querySelector('input');
+(function () {
+    const numbers = document.querySelectorAll('.tile-product .number');
+    numbers.forEach((number) => {
+        number.addEventListener('click', (e) => {
+            const plus = number.querySelector('.plus');
+            const minus = number.querySelector('.minus');
+            const input = number.querySelector('input');
 
-        if (e.target == plus && input.value < 10) {
-            input.value = parseInt(input.value) + 1;
-        } else if (e.target == minus && input.value > 0) {
-            input.value = parseInt(input.value) - 1;
-        }
+            if (e.target == plus && input.value < 10) {
+                input.value = parseInt(input.value) + 1;
+            } else if (e.target == minus && input.value > 0) {
+                input.value = parseInt(input.value) - 1;
+            }
+        })
     })
-})
 })();
 
-(function(){
-const number_cart = document.querySelectorAll('.tile-light .number-light');
-number_cart.forEach((numbers) => {
-    numbers.addEventListener('click', (e) => {
-        const plus_light = numbers.querySelector('.plus-light');
-        const minus_light = numbers.querySelector('.minus-light');
-        const input = numbers.querySelector('input');
+(function () {
+    const number_cart = document.querySelectorAll('.tile-light .number-light');
+    number_cart.forEach((numbers) => {
+        numbers.addEventListener('click', (e) => {
+            const plus_light = numbers.querySelector('.plus-light');
+            const minus_light = numbers.querySelector('.minus-light');
+            const input = numbers.querySelector('input');
 
-        if (e.target == plus_light && input.value < 10) {
-            input.value = parseInt(input.value) + 1;
-        } else if (e.target == minus_light && input.value > 0) {
-            input.value = parseInt(input.value) - 1;
-        }
+            if (e.target == plus_light && input.value < 10) {
+                input.value = parseInt(input.value) + 1;
+            } else if (e.target == minus_light && input.value > 0) {
+                input.value = parseInt(input.value) - 1;
+            }
+        })
     })
-})
 })();
 
-(function(){
-// menu hamburger
-const hamburger = document.querySelector(".button-menu .hamburger");
-const header = document.querySelector(".nav-items");
+(function () {
+    // menu hamburger
+    const hamburger = document.querySelector(".button-menu .hamburger");
+    const header = document.querySelector(".nav-items");
 
-hamburger.addEventListener("click", mobileMenu);
+    hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    header.classList.toggle("active");
-}
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        header.classList.toggle("active");
+    }
 })();
+
 
 //Button scroll
 var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 
@@ -78,7 +79,7 @@ const sliderAnimOptions = {
 
 const sliderData = [
     {
-        image: 'photos/Photo–1.png',   
+        image: 'photos/Photo–1.png',
     },
     {
         image: 'photos/Photo–1.png',
@@ -126,23 +127,19 @@ function plusDivs(n) {
     img.animate(sliderAnim, sliderAnimOptions);
 }
 
-  //Function which allows for hide and show text in class footer-information
-  jQuery(document).ready(function($){
-    op = function(obj) {
-      $(obj).stop().slideToggle();
-      };
-  });
+//Function which allows for hide and show text during responsive pages
+jQuery(document).ready(function ($) {
+    op = function (obj) {
+        $(obj).stop().slideToggle();
+    };
+});
 
 //Function which allows the opinion to be marked stars
-function onMouseEnter(index){
-    var fa = document.getElementsByClassName("fa-star");
-    var j;
-    for(j = 0; j <= fa.length; j++){
-      if(j < index){
-        fa[j].style.color = "orange";
-      }
-      else{
-        fa[j].style.color = "#fde4ae";
-      }
+$(".star").on('click', function(){
+    for(let i=0; i<= 4;i++){
+      document.getElementById(i).querySelector('.fa-star').style.color = "";
     }
-  }
+    for(let i=0; i<= this.id;i++){
+      document.getElementById(i).querySelector('.fa-star').style.color = "orange";
+    }
+  });
