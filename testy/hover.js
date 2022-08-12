9511f65581f27a5e82a3d5e1b463941bb2ba3435
Hubert <hubert.hejnowicz@interia.pl>
2022-08-12 14:30:31 +0200

    Only tests

--- testy/hover.js
+++ testy/hover.js
@@ -1,33 +1,8 @@
-window.onload = function(){
-    slideOne();
-    slideTwo();
-}
-
-let sliderOne = document.getElementById("slider-1");
-let sliderTwo = document.getElementById("slider-2");
-let displayValOne = document.getElementById("range1");
-let displayValTwo = document.getElementById("range2");
-let minGap = 0;
-let sliderTrack = document.querySelector(".slider-track");
-let sliderMaxValue = document.getElementById("slider-1").max;
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt
-    (sliderOne.value) <= minGap){
-        sliderOne.value = parseInt(sliderTwo.value) - minGap;
-    }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
-}
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt
-    (sliderOne.value) <= minGap){
-        sliderTwo.value = parseInt(sliderOne.value) + minGap;
-    }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
-}
-function fillColor(){
-    percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}% , #dadae5 ${percent2}%`;
-}
+$('#open-modal').click(function() {
+    $('.your-modal').show();
+    $('.your-modal').removeClass('animated zoomOut').addClass('animated zoomIn');
+  });
+  
+  $('.close-button').click(function() {
+    $('.your-modal').removeClass('animated zoomIn').addClass('animated zoomOut').delay(800).fadeOut(400);
+  });
