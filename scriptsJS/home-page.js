1eb4bcc99fde5d762fbdf52dbf43f5e814ca1c1d
Hubert <hubert.hejnowicz@interia.pl>
2022-08-17 10:37:19 +0200

    Add carousel on lights

--- scriptsJS/home-page.js
+++ scriptsJS/home-page.js
@@ -169,6 +169,25 @@ function myFunction() {
       };
   });
 
+(function (){
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+ autoplay:true,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+})();
 
 
 
