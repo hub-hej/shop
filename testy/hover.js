1e44b09d036075b903c5dbc020dacecddfaac7c0
Hubert <hubert.hejnowicz@interia.pl>
2022-08-18 14:20:25 +0200

    Only tests

--- testy/hover.js
+++ testy/hover.js
@@ -1,18 +1,6 @@
-
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
- autoplay:true,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:4
-      }
-  }
-})
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
