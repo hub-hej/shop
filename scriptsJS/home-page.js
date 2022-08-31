0fae55e9bf879f8bb8c95b6824c70428df4214a3
Hubert <hubert.hejnowicz@interia.pl>
2022-08-31 12:10:54 +0200

    Next changes on home page

--- scriptsJS/home-page.js
+++ scriptsJS/home-page.js
@@ -193,6 +193,96 @@ $('.owl-carousel').owlCarousel({
 
 
 
+// TU ZACZYNA SIĘ SKRYPT ODNOŚNIE NALICZANIA, DODAWANIA PRZEDMIOTÓW DO KOSZYKA
+let carts = document.querySelectorAll('.button5');
+let products = [
+  {
+    name: 'ŁĄCZNIK JEDNOBIEGUNOWY',
+    tag: 'Kod produktu: CW1.01/11',
+    price: 442.85, 
+    inCart: 0
+  },
+  {
+    name: 'ŁĄCZNIK JEDNOBIEGUNOWY',
+    tag: 'Kod produktu: CW1.01/11',
+    price: 442.85, 
+    inCart: 0
+  },
+  {
+    name: 'ŁĄCZNIK JEDNOBIEGUNOWY',
+    tag: 'Kod produktu: CW1.01/11',
+    price: 442.85, 
+    inCart: 0
+  },
+  {
+    name: 'ŁĄCZNIK JEDNOBIEGUNOWY',
+    tag: 'Kod produktu: CW1.01/11',
+    price: 442.85, 
+    inCart: 0
+  }
+];
+for (let i=0; i< carts.length; i++){
+  carts[i].addEventListener('click', () =>{
+    cartNumbers(products[i]);
+    totalCost(products[i])
+  })
+}
+ function onLoadCartNumbers(){
+   let productNumbers = localStorage.getItem('cartNumbers');
+   if(productNumbers){
+     document.querySelector('.number-orders').textContent = productNumbers;
+   }
+}
+function cartNumbers(product){
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  productNumbers = parseInt(productNumbers);
+  if(productNumbers){
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.number-orders').textContent = productNumbers + 1;
+  }
+  else
+  {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.number-orders').textContent = 1;
+  }
+  setItems(product);
+}
+function setItems(product){
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  if(cartItems != null){
+    if(cartItems[product.tag] == undefined){
+      cartItems = {
+        ...cartItems, 
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product 
+    }
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+function totalCost(product){
+  let cartCost = localStorage.getItem('totalCost');
+
+  if(cartCost != null){
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else{
+    localStorage.setItem("totalCost", product.price);
+  }
+}
+onLoadCartNumbers();
+
+
+
+
 
 
 
