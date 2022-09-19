8417496139d84d3610ec8d0c2a925aa12dfab2c7
Hubert <hubert.hejnowicz@interia.pl>
2022-09-19 13:50:45 +0200

    Add div search to bottom menu responsive

--- scriptsJS/password-reminder.js
+++ scriptsJS/password-reminder.js
@@ -47,3 +47,83 @@ $(document).ready(function() {
       
     });
 });
+
+function search_level() {
+    let input = document.getElementById('myInputs').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('menu_level1');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="flex";                 
+        }
+    }
+  }
+  
+  function openNav() {
+    document.getElementById("myNav").style.width = "70%";
+  }
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  //Open category "Produkty"
+  function openProducts() {
+    document.getElementById("myProducts").style.width = "70%";
+  }
+  function closeProducts() {
+    document.getElementById("myProducts").style.width = "0%";
+  }
+  
+  //Open category "Producenci"
+  function openManufacturers() {
+    document.getElementById("myManufacturers").style.width = "70%";
+  }
+  function closeManufacturers() {
+    document.getElementById("myManufacturers").style.width = "0%";
+  }
+  
+  //Open category "Źródła Światła"
+  function openElements() {
+    document.getElementById("myElementsLights").style.width = "70%";
+  }
+  function closeElements() {
+    document.getElementById("myElementsLights").style.width = "0%";
+  }
+  
+  //Open category "Spark" from "Źródła Światła"
+  function openSpark(){
+    document.getElementById("myCategorySpark").style.width = "70%";
+  }
+  function closeSpark() {
+    document.getElementById("myCategorySpark").style.width = "0%";
+  }
+  
+  //Open category "Automatyka, sterowanie"
+  function openAutomatic(){
+    document.getElementById("myAutomatic").style.width = "70%";
+  }
+  function closeAutomatic() {
+    document.getElementById("myAutomatic").style.width = "0%";
+  }
+  
+  //Open category "Oświetlenie wewnętrzne"
+  function openInteriorLighting(){
+    document.getElementById("myInterior").style.width = "70%";
+  }
+  function closeInteriorLighting() {
+    document.getElementById("myInterior").style.width = "0%";
+  }
+  
+  //Open overlay on whole page related to settings regional
+  function openWholeNav() {
+    document.getElementById("myWholeNav").style.display = "block";
+  }
+  
+  function closeWholeNav() {
+    document.getElementById("myWholeNav").style.display = "none";
+  }
+  
